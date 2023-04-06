66638c0c1c137649039e6a9728a8802c5b188151
Rachkov Roman <pakycb84@gmail.com>
2023-04-06 11:42:38 +1000

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslintPlugin from "vite-plugin-eslint";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), eslintPlugin()],
-});
+})
